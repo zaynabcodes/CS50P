34ab1f033f047e0252f76d916dd802e1e7f8a87a
Zaynab Bennis <z.bennis12@gmail.com>
2023-10-14 13:14:57 -0500

    deleted week9 folder

--- week9/practice11.py
+++
@@ -1,7 +0,0 @@
-#dictionary comprehensions you can create a dictionary on the fly
-
-students = ["Hermonie", "Harry", "Ron"]
-
-gryffindors = {student : "Gryffindor" for student in students}
-
-print(gryffindors)
--- week9/practice12.py
+++
@@ -1,6 +0,0 @@
-#enumerate is a function that iterates through a sequence
-
-students = ["Hermonie", "Harry", "Ron"]
-
-for i, student in enumerate(students):
-    print(i + 1, student)
